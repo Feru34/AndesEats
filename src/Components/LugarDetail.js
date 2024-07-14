340666ee8ee7ced15786e947dbc91f6cba7c71f1
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-07-13 23:49:19 -0500

    Otra mejora estilos comentarios

--- src/Components/LugarDetail.js
+++ src/Components/LugarDetail.js
@@ -16,20 +16,22 @@ const Stars = ({ rating }) => {
 
 const LugarDetail = (props) => {
   return (
-    <div className="info-box">
-      <h1>{props.Nombre}</h1>
-      <Stars rating={props.Rating} />
-      <h3 className="ubicacion" >Dirección: {props.Ubicacion}</h3>
-      <p className="description">{props.Descripcion}</p>
-      <div className="comments">
-        <h3>Comentarios de clientes:</h3>
-        <ul className="comment-list">
-          {props.Comentarios.map((comentario, index) => (
-            <li key={index} className="comment">{comentario}</li>
-          ))}
-        </ul>
-      </div>
-      <button className="close-btn" onClick={() => props.SetActivePoint(null)}>Cerrar</button>
+    <div className="overlay">
+        <div className="info-box">
+        <h1>{props.Nombre}</h1>
+        <Stars rating={props.Rating} />
+        <h3 className="ubicacion" >Dirección: {props.Ubicacion}</h3>
+        <p className="description">{props.Descripcion}</p>
+        <div className="comments">
+            <h3>Comentarios de clientes:</h3>
+            <ul className="comment-list">
+            {props.Comentarios.map((comentario, index) => (
+                <li key={index} className="comment">{comentario}</li>
+            ))}
+            </ul>
+        </div>
+        <button className="close-btn" onClick={() => props.SetActivePoint(null)}>Cerrar</button>
+        </div>
     </div>
   );
 }
