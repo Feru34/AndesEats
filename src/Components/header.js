48a864d076239ad25ee266ce67834b8b89cbf4f3
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-07-14 00:25:11 -0500

    Agregar Logo

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,10 +5,10 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
+      <img className="LogoHeader" src="../../public/2.png" />
       <div className="header-content">
         <nav>
           <ul className="nav-list">
-            <li className="nav-item"><a href="#home">Todo</a></li>
             <li className="nav-item"><a href="#about">Vegetariano</a></li>
             <li className="nav-item"><a href="#services">Almuerzos</a></li>
             <li className="nav-item"><a href="#contact">Meriendas</a></li>
