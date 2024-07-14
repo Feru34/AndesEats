0de3491e171f05e98eef7d86356474d6d458e82e
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-07-14 13:45:21 -0500

    renponsive header mejora

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,14 +1,23 @@
 // src/Components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../3.png';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-content">
-          <img className="LogoHeader" src={logo} alt="Logo"  />
-        <nav>
+        <img className="LogoHeader" src={logo} alt="Logo" />
+        <button className="hamburger-menu" onClick={toggleMenu}>
+          &#9776;
+        </button>
+        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
           <ul className="nav-list">
             <li className="nav-item"><a href="#about">Vegetariano</a></li>
             <li className="nav-item"><a href="#services">Almuerzos</a></li>
